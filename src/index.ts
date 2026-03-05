171954928b14aeb4c8fe52aabf0c2d0b6dac68c0
Mayank Mahajan <mayankmahajan9056@gmail.com>
2026-03-05 18:04:23 +0530

    feat: setup express app with security middleware, rate limiting and cors configuration

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,72 @@
 import express from "express";
+import hpp from "hpp";
+import helmet from "helmet";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
+
+// Global rate limiting
+import { rateLimit } from "express-rate-limit";
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  message: "Too many request from this IP, please try later",
+  standardHeaders: "draft-8", // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+  ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
+  // store: ... , // Redis, Memcached, etc. See below.
+});
+
+// security middleware
+app.use(helmet());
+app.use("/api", limiter);
+app.use(hpp());
+
+// CORS configuration
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+    methods: ["GET", "POST", "PATCH", "DELETE", "HEAD", "OPTIONS"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "X-Requested-With",
+      "device-remember-token",
+      "Access-Control-Allow-Origin",
+      "Origin",
+      "Access",
+    ],
+  })
+);
+
+// Body parser middleware
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(cookieParser());
+
+// Disable ETag generation
+app.disable("etag");
+
+// Also disable browser caching
+app.use((req, res, next) => {
+  res.set("Cache-Control", "no-store, no-cache, must-revalidate, private");
+  next();
+});
+
+// import routes
+
+// routes
+
+// It should be always at bottom
+// 404 handler
+app.use((req, res) => {
+  res.status(404).json({
+    status: "error",
+    message: "Route not found ❌",
+  });
+});
 
 export { app };
